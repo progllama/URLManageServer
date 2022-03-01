415a5737f95c8aeb01663ac4f8bf7036259d424e
llama <progllama.rust@gmail.com>
2022-03-02 01:10:09 +0900

    [ADD] Session middlware.

---
+++ src/app/middlewares/session_middleware.go
@@ -0,0 +1,21 @@
+package middlewares
+
+import (
+	"net/http"
+
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-gonic/gin"
+)
+
+func RequireLogin(c *gin.Context) {
+	session := sessions.Default(c)
+	uid := session.Get("UserId")
+
+	if uid == nil {
+		c.JSON(http.StatusUnauthorized, gin.H{})
+		c.Abort()
+	} else {
+		c.Set("UserId", uid)
+		c.Next()
+	}
+}
