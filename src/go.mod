035e54e77f14d377140f40ca97bb4cce49eba637
llama <progllama.rust@gmail.com>
2022-03-18 02:00:21 +0900

    solve cors.

--- src/go.mod
+++ src/go.mod
@@ -3,6 +3,7 @@ module url_manager
 go 1.15
 
 require (
+	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-contrib/sessions v0.0.4
 	github.com/gin-gonic/gin v1.7.7
 	github.com/jinzhu/gorm v1.9.16
--- src/server/server.go
+++ src/server/server.go
@@ -1,9 +1,11 @@
 package server
 
 import (
+	"time"
 	"url_manager/app/controllers"
 	"url_manager/app/middlewares"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-contrib/sessions/redis"
 	"github.com/gin-gonic/gin"
@@ -21,6 +23,38 @@ func Open(port string) {
 		panic(err)
 	}
 	router.Use(sessions.Sessions("URLManager", store))
+
+	router.Use(cors.New(cors.Config{
+		// 許可したいHTTPメソッドの一覧
+		AllowMethods: []string{
+			"POST",
+			"GET",
+			"OPTIONS",
+			"PUT",
+			"DELETE",
+		},
+		// 許可したいHTTPリクエストヘッダの一覧
+		AllowHeaders: []string{
+			"Access-Control-Allow-Headers",
+			"Content-Type",
+			"Content-Length",
+			"Accept-Encoding",
+			"X-CSRF-Token",
+			"Authorization",
+		},
+		// 許可したいアクセス元の一覧
+		AllowOrigins: []string{
+			"http://localhost:3000",
+		},
+		AllowCredentials: true,
+		// 自分で許可するしないの処理を書きたい場合は、以下のように書くこともできる
+		// AllowOriginFunc: func(origin string) bool {
+		//  return origin == "https://www.example.com:8080"
+		// },
+		// preflight requestで許可した後の接続可能時間
+		// https://godoc.org/github.com/gin-contrib/cors#Config の中のコメントに詳細あり
+		MaxAge: 24 * time.Hour,
+	}))
 
 	router.GET("/", controllers.About)
 	router.GET("/about", controllers.About)
